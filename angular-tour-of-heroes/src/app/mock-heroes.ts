e575e5ed4b3e43b6e7ef5200201005727e27ccaf
EisVollen <lena-loko@mail.ru>
2018-02-24 21:07:52 +0600

    3. Display a Heroes List
    Display a Heroes List

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+{id: 11, name: 'Mr. Nice'},
+{id:12, name: 'Nacro'},
+{id:13, name:'Bpmbasto'},
+{id: 14, name: 'Celeritas'},
+{id: 15, name: 'Astoria'},
+{id: 16, name: 'Iliya'},
+{id: 17, name: 'Walter'},
+{id:18, name: 'Sher'},
+{id:19, name: 'Maddyson'},
+{id:20, name: 'Greengrass'}
+];
